8dacbefb03c76f2abb9c0c891bfce9f436538308
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-02-14 12:22:03 +0200

    Add files via upload

---
+++ ConsoleApp1/Calculate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    /// <summary>
+    /// Class описывающий вычисления.
+    /// </summary>
+    class Calculate
+    {
+        private double field1;
+        private double field2;
+
+        public double Field1 { get => field1;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException ("Error");
+                field1 = value;
+            }
+            }
+        public double Field2 { get => field2; 
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException ("Error");
+                field2 = value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор без параметров(по умолчанию)
+        /// </summary>
+        public Calculate() { }
+        /// Конструктор с одним параметром
+        /// </summary>
+        /// <param name="Поля1"></param>
+        public Calculate(double field1)
+        {
+            this.Field1 = field1;
+
+        }
+        /// <summary>
+        /// Конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="Поля2"></param>
+        public Calculate (double field1,double field2) :this(field1)
+        {
+            this.Field2 = field2;
+        }
+        /// <summary>
+        /// Функция формирования строки с информацией об объекте.
+        /// </summary>
+        
+        public void Output1() // вариант 1 
+        {
+            Console.WriteLine($"field1 = {Field1}\t field2 = {Field2}\t корень: {Func()} ");
+        }
+        public override string ToString() //вариант 2
+        {
+            return $"field1 = {Field1}\t field2 = {Field2} корень: {Func()}";
+        }
+        /// <summary>
+        /// Функция обработки значений полей: вычислить корень квадратный из произведения чисел.
+        /// </summary>
+        public double Func()
+        {
+            return Math.Sqrt(Field1 * Field2);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc = new Calculate();
+            calc.Field1 = 3;
+            calc.Field2 = 10;
+            calc.Output1();
+            Calculate calc1 = new Calculate(1);
+            calc1.Output1();
+            Calculate calc2 = new Calculate(3, 4);
+            calc2.Output1();
+            Console.WriteLine("Введите первое число:");
+            double asd1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ведите второе число:");
+            double asd2 = double.Parse(Console.ReadLine());
+            Calculate calc3 = new Calculate(asd1,asd2);
+            Console.WriteLine($"{calc3.ToString()}");
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
